46bef9ee1d9c324ae12429ff6abbf04a19be1291
Vlad <miass28@mail.ru>
2023-06-13 21:45:17 +0500

    изменил цикл

--- lesson1/home8/Program.cs
+++ lesson1/home8/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите число:");
 int m = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i<=m; i++)
+//for (int i = 1; i<=m; i++)
+for (int i = 2; i<=m; i+=2)
 {
-    if(i % 2 == 0) Console.WriteLine(i);
+//    if(i % 2 == 0) Console.WriteLine(i);
+    Console.WriteLine(i);
 }
