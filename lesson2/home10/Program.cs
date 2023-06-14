11625b303d84734f4660a479a931e1305cfb0f3d
Vlad <miass28@mail.ru>
2023-06-14 14:29:49 +0500

    выполнено домашнее здание

---
+++ lesson2/home10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число:");
+string? numberString = Console.ReadLine()!;
+int numberInt = Convert.ToInt32(numberString);
+numberInt = (numberInt - numberInt / 100 * 100) / 10;
+Console.WriteLine(numberInt);
---
+++ lesson2/home15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите день недели:");
+string? numberString = Console.ReadLine()!;
+int numberInt = Convert.ToInt32(numberString);
+switch(numberInt)
+{
+    case 1:
+        Console.WriteLine("нет");
+        break;
+    case 2:
+        Console.WriteLine("нет");
+        break;
+    case 3:
+        Console.WriteLine("нет");
+        break;
+    case 4:
+        Console.WriteLine("нет");
+        break;
+    case 5:
+        Console.WriteLine("нет");
+        break;
+    case 6:
+        Console.WriteLine("да");
+        break;
+    case 7:
+        Console.WriteLine("да");
+        break;
+    default:
+        Console.WriteLine("такого дня нет");
+        break;
+}
