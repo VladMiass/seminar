120f3895f0d391c9fbcafeb792d327ac77bb4f9d
Vlad <miass28@mail.ru>
2023-06-16 23:45:06 +0500

    решил задач 21 и 23

---
+++ lesson3/home21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите координату х первой точки:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату y первой точки:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату z первой точки:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату х второй точки:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату y второй точки:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите координату z второй точки:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+Console.WriteLine("S = " + s);
