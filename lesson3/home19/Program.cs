227e7febca87ead29893675f5b7d9b62d60d04b1
Vlad <miass28@mail.ru>
2023-06-17 20:32:23 +0500

    решил home19

---
+++ lesson3/home19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите пятизначное число:");
+int n5 = Convert.ToInt32(Console.ReadLine());
+int nn = n5;
+int res = 0;
+for(int i = 0; i < 5; i++)
+{
+    res = res * 10 + n5 % 10;
+    n5 = n5 / 10;
+    Console.WriteLine(i + "  " + n5 + "  " + res);
+}
+if(nn == res) Console.WriteLine(" палиндром ");
+else Console.WriteLine("  не палиндром ");
