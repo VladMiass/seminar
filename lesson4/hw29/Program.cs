6e11ba839d76897927875c47fd8bd590baa27e22
Vlad <miass28@mail.ru>
2023-06-21 09:15:18 +0500

    добавил урок 4

---
+++ lesson4/hw29/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string msg)
+{ 
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array = new int[8];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = ReadInt("Ввести " + (i + 1) + " элемент: ");
+}
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + "  ");
+}
