dfbb60cd19fed3da3d250cb9957ecb5596abb6b5
Vlad <miass28@mail.ru>
2023-07-01 00:01:41 +0500

    семинар 7

---
+++ lesson7/task49/Program.cs
@@ -0,0 +1,46 @@
+﻿const int ROWS = 3;
+const int COLUMNS = 4;
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 != 0 && j % 2 != 0) matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+        
+    }
+}
+
+int[,] myMatrix = GetMatrix(ROWS, COLUMNS);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+ChangeMatrix(myMatrix);
+PrintMatrix(myMatrix);
---
+++ lesson7/task51/Program.cs
@@ -0,0 +1,43 @@
+﻿const int ROWS = 3;
+const int COLUMNS = 4;
+
+int[,] GetMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int DiagMatrix(int[,] matrix)
+{
+    int sum = 0;
+    int len = Math.Min(matrix.GetLength(0), matrix.GetLength(1));
+    for(int i = 0; i < len; i++)
+    {
+        sum = sum + matrix[i, i];
+    }
+    return sum;
+}
+
+int[,] myMatrix = GetMatrix(ROWS, COLUMNS);
+PrintMatrix(myMatrix);
+Console.WriteLine(DiagMatrix(myMatrix));
