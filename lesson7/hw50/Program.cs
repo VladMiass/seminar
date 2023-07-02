d42bfac884f8288d48ffe3cd77951de5146b77ac
Vlad <miass28@mail.ru>
2023-07-02 13:08:12 +0500

    семинар 7, все решено

---
+++ lesson7/hw50/Program.cs
@@ -0,0 +1,38 @@
+﻿const int ROWS = 3;
+const int COLUMNS = 4;
+
+float[,] GetRandomArray(int rows, int columns)
+{
+    float[,] matrix = new float[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Random.Shared.NextSingle() * 100;
+        }     
+    }
+    return matrix;
+}
+
+void PrintMatrix(float[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+float[,] myMatrix = GetRandomArray(ROWS, COLUMNS);
+PrintMatrix(myMatrix);
+Console.WriteLine("Введи через пробел номер строки и столбца");
+string[] mn = Console.ReadLine().Split(" ");
+int m = Convert.ToInt32(mn[0]);
+int n = Convert.ToInt32(mn[1]);
+if(m >= 0 && m <= ROWS && n >= 0 && n <= COLUMNS)
+    Console.WriteLine("Элемент строка " + m + " столбец " + n + " равен " + myMatrix[m, n]);
+else
+    Console.WriteLine("Такого элемента нет");
---
+++ lesson7/hw52/Program.cs
@@ -0,0 +1,47 @@
+﻿const int ROWS = 3;
+const int COLUMNS = 4;
+
+int[,] GetRandomArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = Random.Shared.Next(1,100);
+        }   
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+float[] AverageColumns(int[,] matrix, int columns)
+{
+    float[] array = new float[columns];
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        array[j] = 0;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            array[j] = array[j] + matrix[i, j];
+        }
+        array[j] = array[j] / matrix.GetLength(0);
+    }
+    return array;
+}
+
+int[,] myMatrix = GetRandomArray(ROWS, COLUMNS);
+float[] arrayAverage = AverageColumns(myMatrix, COLUMNS);
+PrintMatrix(myMatrix);
+Console.WriteLine(string.Join(",  ", arrayAverage));
